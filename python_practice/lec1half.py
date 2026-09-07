6d060c8869ff9f9ee902511de8d44e0780385bb6
Shahidul Alif <shahidulalif03@gmail.com>
2026-09-07 16:41:27 +0600

    Create lec1half.py

---
+++ python_practice/lec1half.py
@@ -0,0 +1,40 @@
+print("Hello world")
+#for short
+#example
+print("I am shahidul Alif.")
+print("I am a software engineer.")
+#if we want to short then we can use this
+print("I am shahidul Alif.","I am a software engineer.")
+#print any number
+print(34)
+#variable
+#A variable is a name given by memory location in a program
+name="Alif"
+age= 22
+cgpa=3.88
+print(name)
+print(age)
+print(cgpa)
+print("My name is : ",name)
+#if we want to see variable types
+print(type(name))
+print(type(age))
+print(type(cgpa))
+#string can be written inside of
+name1='alif'
+name2="ali"
+name3='''alf'''
+print(name1)
+print(name2)
+print(name3)
+#boolean
+age=23
+old=False
+a=None
+print(type(old))
+print(type(a))
+#print sum
+a=2
+b=4
+sum=a+b
+print(a+b)
